d86e3c63b108d5c74dcf3ff3063e23543a7f2515
ivanhrushka <ivanhrushka.py@gmail.com>
2017-02-21 08:53:25 +0200

    FFF

--- chatroom/views.py
+++ chatroom/views.py
@@ -2,21 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 
-from .forms import ProfileForm
-
 
 class MainPage(TemplateView):
     template_name = "chatroom/base.html"
-
-
-class RegistrationView(FormView):
-    template_name = "registration/registration_form.html"
-    form_class = ProfileForm
-
-    success_url = "/"
-
-    def form_valid(self, form):
-        form.save()
-        return super(RegistrationView, self).form_valid(form)
-
 
